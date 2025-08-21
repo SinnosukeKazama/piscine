413afb0ab515d3788d188f0b35614aa8b88a4c86
skazama <wingthingsk@gmail.com>
2025-08-21 12:18:10 +0900

    remake: ft_putnbr.c

--- c04/ex02/ft_putnbr.c
+++ c04/ex02/ft_putnbr.c
@@ -4,21 +4,55 @@ void ft_putnbr(int nb)
 	int i;
 	int mod;
 	char s[0xff];
+	int	minus_flag;
 
-	i = 0;
-	while (nb > 0) /* n を削る */
+	minus_flag = 0;
+	if (nb == -2147483647)//この数が最大なのはvs設定？仕様？？
+		printf("-2147483648");
+	else if (nb == 0)
+		printf("0");
+	else if (nb < 0 && minus_flag == 0)
+	{
+		nb *= -1;
+		minus_flag = 1;
+	}
+	i = -1;
+	while (nb > 0)
 	{
 		mod = nb % radix;
-		s[i] = mod + '0'; /* 次の桁 */
 		nb /= radix;
-		++i;
+		s[++i] = mod + '0';
 	}
-	while(--i >= 0)
-		printf("%c", s[i]);//mast swap to write()
+	if (minus_flag == 1)
+		printf("-");
+	while(i >= 0)
+		printf("%c", s[i--]);//mast swap to write()
+}//26行！！！！！！！！！！
+
+
+void	ft_putnbr_rec(int nb)
+{
+	const int	radix = 10;
+
+	//終了条件１
+	if (nb ==  - 2147483647)//この数が最大なのはvs設定？仕様？？
+		printf("-2147483648");
+	//終了条件２
+	if (nb == 0)
+		printf("0");
+
+	else if (nb < 0)
+	{
+		nb *= -1;
+	}
+	if(nb > 0)
+	{
+
+	}
 }
 
 #include<stdio.h>
 int main()
 {
-	ft_putnbr(43567);
+	ft_putnbr(756);
 }
